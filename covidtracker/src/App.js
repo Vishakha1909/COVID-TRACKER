835afc32766de448efe2beb5935b5b9443ad3e36
Vishakha Kumaresan <vishakhakumaresan2001@gmail.com>
2020-12-23 02:20:56 +0530

    creating left and right containers

--- covidtracker/src/App.js
+++ covidtracker/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import { MenuItem,FormControl,Select} from "@material-ui/core";
+import { MenuItem,FormControl,Select,Card,CardContent} from "@material-ui/core";
 import {useState,useEffect} from "react";
 import InfoBox from './InfoBox';
+import Map from './Map';
 //https://disease.sh/v3/covid-19/countries
 
 function App() {
@@ -33,36 +34,42 @@ function App() {
 
   return (
     <div className="app">
-      <div className="app_header">
-        <h1>COVID19 TRACKER</h1>
-        <FormControl className="app_dropdown">
-          <Select variant="outlined" onChange={onCountryChange} value={country}>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {
-              countries.map((country) => (
-                <MenuItem value={country.value}>{country.name}</MenuItem>
-              ))
-            }
-  
-          </Select>
-        </FormControl>
-      </div>
-
-      <div className="app_stats">
-        <InfoBox title="Corona Virus Cases"/>
-        <InfoBox title="Corona Virus Recoveries"/>
-        <InfoBox title="Corona Virus Deaths"/>
-        {/*Infobox title="CoronaVirusCases"*/}
-        {/*Infobox title="recoveries"*/}
-        {/*Infobox* title="deaths"*/}
-      </div>
-      
-
+      <div className="app_left">
+        <div className="app_header">
+          <h1>COVID19 TRACKER</h1>
+          <FormControl className="app_dropdown">
+            <Select variant="outlined" onChange={onCountryChange} value={country}>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {
+                countries.map((country) => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))
+              }
     
-      {/*Table*/}
-      {/*Graph*/}
+            </Select>
+          </FormControl>
+        </div>
 
-      {/*Map*/}
+        <div className="app_stats">
+          <InfoBox title="Corona Virus Cases" cases={123} total={123}/>
+          <InfoBox title="Corona Virus Recoveries" cases={123} total={123}/>
+          <InfoBox title="Corona Virus Deaths" cases={123} total={123}/>
+        </div>
+
+        {/*Map*/}
+        <Map/>
+      </div>
+
+      <Card className="app_right">
+        <CardContent>
+          <h3>Live Cases</h3>
+          <h3>Graph</h3>
+          {/*Table*/}
+          {/*Graph*/}
+        </CardContent>
+      </Card>
+      
+      
     </div>
   );
 }
