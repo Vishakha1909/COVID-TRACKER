c78249bcd85abe1771844012c093e49edeccc5a0
Vishakha Kumaresan <vishakhakumaresan2001@gmail.com>
2020-12-23 01:28:42 +0530

    menu created

--- covidtracker/src/App.js
+++ covidtracker/src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
-import {
-  MenuItem,
-  FormControl,
-  Select
-} from "@material-ui/core";
+import { MenuItem,FormControl,Select} from "@material-ui/core";
+import {useState} from "react";
 
 function App() {
+  const [countries,setCountries] = useState(['USA','UK']);
+
+
   return (
     <div className="app">
       <div className="app_header">
         <h1>COVID19 TRACKER</h1>
         <FormControl className="app_dropdown">
-          <Select
-            variant="outlined"
-            value="abc"
-          >
-            <MenuItem value="worldwide">OLA</MenuItem>
-            <MenuItem value="worldwide">OLA</MenuItem>
-            <MenuItem value="worldwide">OLA</MenuItem>
-
+          <Select variant="outlined" value="abc">
+            {
+              countries.map((country) => (
+                <MenuItem value={country}>{country}</MenuItem>
+              ))
+            }
+  
           </Select>
         </FormControl>
       </div>
