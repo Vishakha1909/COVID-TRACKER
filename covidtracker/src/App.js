69110e08a9d252a3865e01e6d9bd0a0c236ac857
Vishakha Kumaresan <vishakhakumaresan2001@gmail.com>
2020-12-23 01:59:54 +0530

    info box created

--- covidtracker/src/App.js
+++ covidtracker/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { MenuItem,FormControl,Select} from "@material-ui/core";
 import {useState,useEffect} from "react";
-
+import InfoBox from './InfoBox';
 //https://disease.sh/v3/covid-19/countries
 
 function App() {
   const [countries,setCountries] = useState([]);
+  const [country,setCountry] = useState("worldwide");
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -24,13 +25,19 @@ function App() {
     getCountriesData();
   }, []);
 
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  }
+
 
   return (
     <div className="app">
       <div className="app_header">
         <h1>COVID19 TRACKER</h1>
         <FormControl className="app_dropdown">
-          <Select variant="outlined" value="abc">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {
               countries.map((country) => (
                 <MenuItem value={country.value}>{country.name}</MenuItem>
@@ -40,14 +47,18 @@ function App() {
           </Select>
         </FormControl>
       </div>
+
+      <div className="app_stats">
+        <InfoBox title="Corona Virus Cases"/>
+        <InfoBox title="Corona Virus Recoveries"/>
+        <InfoBox title="Corona Virus Deaths"/>
+        {/*Infobox title="CoronaVirusCases"*/}
+        {/*Infobox title="recoveries"*/}
+        {/*Infobox* title="deaths"*/}
+      </div>
       
-      {/* Header */}
-      {/*Title + Select Input */}
 
-      {/*Infobox*/}
-      {/*Infobox*/}
-      {/*Infobox*/}
-
+    
       {/*Table*/}
       {/*Graph*/}
 
